86abd1fb171eb5ebe0d9ef78754526d7a19d1bed
salvaH21 <sholivares90@gmail.com>
2024-01-26 09:26:04 +0100

    deteccion de movimiento

---
+++ 002 - Interfaces heredadas/001 - Linea de comandos.py
@@ -0,0 +1,4 @@
+#Interfaz de linea de comandos
+print("Este programa va a calcular el docble de tu edad")
+edad = input("Ahora introduce tu edad: ")
+print("el doble de tu edad es de " + str(int(edad)*2) + " años")
---
+++ 002 - Interfaces heredadas/002 - Tkinter.py
@@ -0,0 +1,6 @@
+#Interfaz gráfica de usuario
+import tkinter as tk
+
+raiz = tk.Tk()
+tk.Button(raiz,text="Pulsame").pack(padx=20,pady=20)
+raiz.mainloop()
